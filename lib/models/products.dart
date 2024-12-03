562f3976c117c14af6fea60af61fa1b5605cabde
Wilfer Zuluaga <wdzuluaga54@misena.edu.co>
2024-12-02 22:49:59 -0500

    I must enjoy torturing myself

--- lib/models/products.dart
+++ lib/models/products.dart
@@ -1,19 +1,17 @@
-
 class Product {
   final String id;
   final String name;
   final String description;
   final num price;
   final int stock;
-  final String category;
-
+  final Object category;
 
   Product({
     required this.id,
     required this.name,
     required this.description,
     required this.price,
-    required this.stock,  
+    required this.stock,
     required this.category,
   });
 
@@ -24,18 +22,18 @@ class Product {
       description: json['description'] ?? '', 
       price: json['price'] ?? 0, 
       stock: json['stock'] ?? 0, 
-      category: json['category'],
+      category: json['category'] ?? {},
     );
-  } 
+  }
 
-  Map<String, dynamic> toJson () {
+  Map<String, dynamic> toJson() {
     return {
-      '_id' : id,
-      'name' : name,
-      'description' : description,
-      'price' : price,
-      'stock' : stock,
-      'category' : category,
+      '_id': id,
+      'name': name,
+      'description': description,
+      'price': price,
+      'stock': stock,
+      'category': category,
     };
   }
 }
